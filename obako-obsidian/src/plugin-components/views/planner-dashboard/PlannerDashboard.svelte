03352add1c293ab9b2efd1afcecf035e1c9a3edd
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2024-12-27 11:00:26 +0000

    reloads when a file changes properly now

--- obako-obsidian/src/plugin-components/views/planner-dashboard/PlannerDashboard.svelte
+++ obako-obsidian/src/plugin-components/views/planner-dashboard/PlannerDashboard.svelte
@@ -1,13 +1,15 @@
 <script lang="ts">
 	import PlannerTimeline from "./PlannerTimeline.svelte";
 	import FrontmatterCheckbox from "../../../svelte/FrontmatterCheckbox.svelte";
-
+	import { writable } from "svelte/store";
+	
 	import { registerOnModify } from "../../../internal-utils";
 	import { getNotes } from "../../../utils";
 	import Planner from "../../../notes/planner";
 
 	const planners = getNotes("planner") as Planner[];
-
+	const reloadKey = writable(0); 
+	
 	function plannerActiveCheckboxChanged(planner: Planner) {
 		planner.active = !planner.active;
 	}
@@ -18,11 +20,14 @@
 			const updatedPlanners = getNotes("planner") as Planner[];
 			planners.length = 0; 
 			planners.push(...updatedPlanners);
+			reloadKey.update(n => n + 1); // Update the key to trigger reload
 		}, 10);
 	});
 </script>
 
-<PlannerTimeline />
+{#key $reloadKey}
+	<PlannerTimeline />
+{/key}
 
 <h2>Active planners</h2>
 
