7baed5b3d221131dc3f34e6a76be6f804bcac466
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2024-12-30 15:14:50 +0000

    fixed inline-title-decorator

--- obako-obsidian/src/plugin-components/ui/inline-title-decorator.ts
+++ obako-obsidian/src/plugin-components/ui/inline-title-decorator.ts
@@ -4,8 +4,9 @@
 
 import { MarkdownView, TFile } from 'obsidian';
 import { loadNote } from 'src/note-loader';
-import BasicNote from 'src/notes/basic-note';
+import { BasicNote } from 'src/notes/basic-note';
 import PluginComponent from '../plugin-component';
+import { around } from 'monkey-around';
 
 const PREFIX_DECORATOR_ID = "obako-inline-title-prefix-decorator";
 const SUFFIX_DECORATOR_ID = "obako-inline-title-suffix-decorator";
@@ -13,49 +14,42 @@ const DECORATOR_CONTAINER_ID = "obako-inline-title-decorator-container";
 
 export class UI_InlineTitleDecorator extends PluginComponent {
     load() {
-        this.app.workspace.onLayoutReady(() => {
-            this.updateInlineTitleDecorator();
-        });
-
-        // Moving between notes
-        this.plugin.registerEvent(
-            this.app.workspace.on("active-leaf-change", () => {
-                this.updateInlineTitleDecorator();
-            })
-        );
-
-        // Moving between source and preview
-        this.plugin.registerEvent(
-            this.app.workspace.on("layout-change", () => {
-                this.updateInlineTitleDecorator();
-            })
-        );
-
         // If the frontmatter changes
         this.plugin.registerEvent(
-            this.app.metadataCache.on("changed", () => {
-                this.updateInlineTitleDecorator();
+            this.app.metadataCache.on("changed", (file, data, cache) => {
+                //this.updateInlineTitleDecorator();
+                const leaf = this.app.workspace.getActiveViewOfType(MarkdownView);
+                if (leaf?.file?.path === file.path) {
+                    this.updateInlineTitleDecorator(leaf.containerEl, file.path);
+                }
             })
         );
 
-        this.plugin.registerEvent(
-            this.app.workspace.on('file-open', (file: TFile) => {
-                this.updateInlineTitleDecorator(); 
+        const self = this;
+
+        this.plugin.register(
+            around(MarkdownView.prototype, {
+                onLoadFile(next) {
+                    return function(...args) {
+                        self.createInlineTitleDecorator(this.containerEl, this.leaf.view.file.path);
+                        return next.call(this, ...args)
+                    }
+                }
             })
-        );
+        )
     }
 
     unload() { }
 
-    updateInlineTitleDecorator() {
-        const leaf = this.app.workspace.getActiveViewOfType(MarkdownView);
-        if (!leaf) return;
-        const note: BasicNote = loadNote(leaf.file); 
+    createInlineTitleDecorator(containerEl: HTMLElement, filePath: string) {
+        if (!containerEl) return;
+        const note: BasicNote | null = loadNote(filePath); 
         if (!note) return;
-    
+
         // Put title into a container
-        let titleContainerEl = leaf.contentEl.querySelector(`#${DECORATOR_CONTAINER_ID}`);
-        const inlineTitleEl = leaf.contentEl.querySelector('.inline-title');
+        let titleContainerEl = containerEl.querySelector(`#${DECORATOR_CONTAINER_ID}`);
+        
+        const inlineTitleEl = containerEl.querySelector('.inline-title');
         if (!inlineTitleEl) return;
         if (!titleContainerEl) {
             titleContainerEl = document.createElement('div');
@@ -64,10 +58,6 @@ export class UI_InlineTitleDecorator extends PluginComponent {
             titleContainerEl.appendChild(inlineTitleEl);
             inlineTitleEl.style.display = 'inline'; // Make sure the inline title is inline so that we can prepend the decorator
         }
-    
-        // Remove existing decorator if present
-        document.querySelectorAll(`#${PREFIX_DECORATOR_ID}`).forEach(el => el.remove());
-        document.querySelectorAll(`#${SUFFIX_DECORATOR_ID}`).forEach(el => el.remove());
         
         // Create the prefix decorator element and prepend it to the inline title
         const inlineTitlePrefixDecoratorEl = document.createElement('div');
@@ -88,6 +78,19 @@ export class UI_InlineTitleDecorator extends PluginComponent {
             inlineTitleSuffixDecoratorEl.style.marginLeft = '10px';
         }
         titleContainerEl.appendChild(inlineTitleSuffixDecoratorEl);
+    }
+
+    updateInlineTitleDecorator(containerEl: HTMLElement, filePath: string) {
+        const note: BasicNote | null = loadNote(filePath); 
+        if (!note) return;
+
+        const inlineTitlePrefixDecoratorEl: HTMLElement | null = containerEl.querySelector(`#${PREFIX_DECORATOR_ID}`);
+        const inlineTitleSuffixDecoratorEl: HTMLElement | null = containerEl.querySelector(`#${SUFFIX_DECORATOR_ID}`);
+
+        if (inlineTitlePrefixDecoratorEl)
+            note.setTitlePrefixDecorator(inlineTitlePrefixDecoratorEl);
+        if (inlineTitleSuffixDecoratorEl)
+            note.setTitleSuffixDecorator(inlineTitleSuffixDecoratorEl);
     }
 };
 
