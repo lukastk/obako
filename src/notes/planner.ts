e58bb61f040388285614ab6a194a34f548d44bb4
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-01-28 12:38:05 +0000

    quarter planners now show month range

--- src/notes/planner.ts
+++ src/notes/planner.ts
@@ -64,6 +64,10 @@ export class Planner extends ObakoNote {
                     const weekStart = `${this.date.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' })}`;
                     const weekEnd = `${this.endDate.toLocaleDateString('en-US', { month: '2-digit', day: '2-digit' })}`;
                     return `${weekStart}-${weekEnd}`;
+                case 'quarter':
+                    const monthStart = `${this.date.toLocaleDateString('en-US', { month: '2-digit' })}`;
+                    const monthEnd = `${this.endDate.toLocaleDateString('en-US', { month: '2-digit' })}`;
+                    return `${monthStart}-${monthEnd}`;
             }
         }
 
