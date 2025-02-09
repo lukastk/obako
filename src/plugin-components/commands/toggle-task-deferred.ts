debbaadf1fb7988f905e633d450c9f6d5b441266
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-02-09 17:11:40 +0000

    toggles proxies and non-tasks as wel

--- src/plugin-components/commands/toggle-task-deferred.ts
+++ src/plugin-components/commands/toggle-task-deferred.ts
@@ -16,6 +16,7 @@ export class Command_ToggleTaskDeferred extends Command_TransformTaskStatus {
                     '>': ' ',
                     ' ': '>',
                     'd': '>',
+                    '+': '>',
                 };
                 const leaf = this.app.workspace.getActiveViewOfType(MarkdownView);
                 const selectionPositions = getSelectionPositions(leaf?.leaf.view.editor);
