d085fc08c2a41c4dda0cc232e82d2b98ebf84f92
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-03-18 21:06:47 +0000

    feat: pad.is-in-writing

--- src/notes/zettel-types/pad.ts
+++ src/notes/zettel-types/pad.ts
@@ -1,3 +1,4 @@
+import type { FrontmatterSpec } from '../note-frontmatter';
 import { Transient } from './transient';
 
 export class Pad extends Transient {
@@ -5,4 +6,22 @@ export class Pad extends Transient {
     static noteTypeStr = "pad";
     static noteTypeDisplayName = "Pad";
     static noteIcon = "✎";
+
+    static getFrontmatterSpec(): FrontmatterSpec {
+        const spec: FrontmatterSpec = {
+            ...super.getFrontmatterSpec(),
+            "pad-in-writing": { default: true, type: "boolean", skipCreationIfAbsent: false, hideInCreationModal: true, description: "Whether the pad is in writing." },
+        };
+        spec.notetype.default = this.noteTypeStr;
+        spec['is-hp-cons'].default = true;
+        return spec;
+    }
+
+    get inWriting(): boolean {
+        return this.frontmatter["pad-in-writing"];
+    }
+
+    get needsConsolidation(): boolean {
+        return !this.consolidated && !this.inWriting;
+    }
 }
