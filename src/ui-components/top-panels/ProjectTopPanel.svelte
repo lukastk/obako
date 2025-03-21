7f5c2bad9cd3683953d0065d08c9deec7e3e5268
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-03-21 11:41:40 +0000

    feat: updated project top panel

--- src/ui-components/top-panels/ProjectTopPanel.svelte
+++ src/ui-components/top-panels/ProjectTopPanel.svelte
@@ -4,37 +4,83 @@
 	import Collapsible from "src/ui-components/svelte-lib/Collapsible.svelte";
 	import LogDashboard from "src/ui-components/dashboards/log/LogDashboard.svelte";
 	import { Log } from "src/notes/zettel-types/log";
-
-	import type { Project } from "src/notes/zettel-types/project";
+	import { Project } from "src/notes/zettel-types/project";
+	import { Doc } from "src/notes/zettel-types/doc";
+	import { Pad } from "src/notes/zettel-types/pad";
 	export let note: Project;
 
 	const incomingLinkedLogs = note
 		.getIncomingLinkedNotes()
 		.filter((note) => note.noteType === Log.noteTypeStr);
-
-	const modules = note.getModules().sort((a, b) => {
-		if (a.status && b.status) return a.status.localeCompare(b.status);
-		else return -1;
-	});
 </script>
 
 <CollapsibleNoteTreeDisplay
-	displayTitle="Child projects"
+	displayTitle="Active child projects"
 	noteTree={note.getDescendantProjects()}
 	isCollapsed={false}
 	displayTitleDecorator={true}
 	sortFunc={(a, b) => {
-		if (a.note.status && b.note.status) return a.note.status.localeCompare(b.note.status);
+		if (a.note.status && b.note.status)
+			return a.note.status.localeCompare(b.note.status);
 		else return -1;
 	}}
+	filterFunc={(noteTree) =>
+		[Project.statuses.active, Project.statuses.stream].includes(
+			noteTree.note.status,
+		)}
 />
 
 <CollapsibleNoteList
 	title="Modules"
-	notes={modules}
+	notes={note.getModules().sort((a, b) => {
+		if (a.status && b.status) return a.status.localeCompare(b.status);
+		else return -1;
+	})}
 	isCollapsed={false}
 	groupByNoteType={false}
 	displayTitleDecorator={true}
+/>
+
+<CollapsibleNoteList
+	title="Docs"
+	notes={note
+		.getIncomingLinkedNotes()
+		.filter((note) => note instanceof Doc)
+		.sort((a, b) => {
+			if (a.status && b.status)
+				return Doc.statusOrder[a.status] - Doc.statusOrder[b.status];
+			else return -1;
+		})}
+	isCollapsed={false}
+	groupByNoteType={false}
+	displayTitleDecorator={true}
+/>
+
+<CollapsibleNoteList
+	title="Pads"
+	notes={note
+		.getIncomingLinkedNotes()
+		.filter((note) => note instanceof Pad)
+		.sort((a, b) => {
+			if (a.inWriting && !b.inWriting) return -1;
+			else if (!a.inWriting && b.inWriting) return 1;
+			else return 0;
+		})}
+	isCollapsed={false}
+	groupByNoteType={false}
+	displayTitleDecorator={true}
+/>
+
+<CollapsibleNoteTreeDisplay
+	displayTitle="All child projects"
+	noteTree={note.getDescendantProjects()}
+	isCollapsed={true}
+	displayTitleDecorator={true}
+	sortFunc={(a, b) => {
+		if (a.note.status && b.note.status)
+			return a.note.status.localeCompare(b.note.status);
+		else return -1;
+	}}
 />
 
 <CollapsibleNoteTreeDisplay
