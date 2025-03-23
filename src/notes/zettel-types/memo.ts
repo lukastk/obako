cb2bf6029ed4eb02c1acb1cfbddfeb5cf4f6783a
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-03-23 20:26:16 +0000

    feat: memo-complete

--- src/notes/zettel-types/memo.ts
+++ src/notes/zettel-types/memo.ts
@@ -1,3 +1,4 @@
+import type { FrontmatterSpec } from '../note-frontmatter';
 import { Zettel } from '../zettel';
 
 export class Memo extends Zettel {
@@ -5,4 +6,13 @@ export class Memo extends Zettel {
     static noteTypeStr = "memo";
     static noteTypeDisplayName = "Memo";
     static noteIcon = "◆";
+
+    static getFrontmatterSpec(): FrontmatterSpec {
+        const spec: FrontmatterSpec = {
+            ...super.getFrontmatterSpec(),
+            "memo-complete": { default: false, type: "boolean", skipCreationIfAbsent: false, hideInCreationModal: true, description: "Whether the memo is complete." },
+        };
+        spec.notetype.default = this.noteTypeStr;
+        return spec;
+    }
 }
