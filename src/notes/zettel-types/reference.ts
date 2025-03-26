e3989bb47b243ad545b7685f73dc3682769ad475
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-03-26 23:06:56 +0000

    feat: ref author and url

--- src/notes/zettel-types/reference.ts
+++ src/notes/zettel-types/reference.ts
@@ -17,9 +17,10 @@ export class Reference extends Source {
     static getFrontmatterSpec(): FrontmatterSpec {
         const spec: FrontmatterSpec = {
             ...super.getFrontmatterSpec(),
+            "author": { default: "", type: "string", description: "The author of the reference." },
+            "url": { default: null, type: "string", description: "The URL of the reference." },
             "ref-status": { default: Reference.statuses.unread, type: "string", description: "The status of the reference." },
             "read-date": { default: null, type: "date", description: "The date the reference was read." },
-            "url": { default: null, type: "string", description: "The URL of the reference." },
             "is-reproduction": { default: false, type: "boolean", skipCreationIfAbsent: false, hideInCreationModal: false, description: "Whether the note is a reproduction of the reference." },
         };
         spec.notetype.default = this.noteTypeStr;
