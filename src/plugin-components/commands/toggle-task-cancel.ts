18aa971bf08ff20e859a4f5c3014202a92ff15cf
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-11-20 21:32:02 +0000

    feat: 'Toggle task cancel' change

--- src/plugin-components/commands/toggle-task-cancel.ts
+++ src/plugin-components/commands/toggle-task-cancel.ts
@@ -16,6 +16,7 @@ export class Command_ToggleTaskCancel extends Command_TransformTaskStatus {
                     '-': ' ',
                     ' ': '-',
                     'd': '-',
+                    'x': '-',
                 };
                 const leaf = this.app.workspace.getActiveViewOfType(MarkdownView);
                 const selectionPositions = getSelectionPositions(leaf?.leaf.view.editor);
